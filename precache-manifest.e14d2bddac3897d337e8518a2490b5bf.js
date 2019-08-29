326ce7b7a25e9b9d2ce4d3600c80d3362798f10c
Jonathan Emery <jonathane@carsons.local>
2019-08-29 09:29:55 -0400

    Updates

---
+++ precache-manifest.e14d2bddac3897d337e8518a2490b5bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "525f51f4a80924439ea4684f6cef0efd",
+    "url": "/MovieSearchv2/index.html"
+  },
+  {
+    "revision": "afe07b619853b3e2efcb",
+    "url": "/MovieSearchv2/static/css/main.22b57fc8.chunk.css"
+  },
+  {
+    "revision": "a994f6649c51ac63a763",
+    "url": "/MovieSearchv2/static/js/2.dc9cd28d.chunk.js"
+  },
+  {
+    "revision": "afe07b619853b3e2efcb",
+    "url": "/MovieSearchv2/static/js/main.791908f7.chunk.js"
+  },
+  {
+    "revision": "3166e920bf923c56ba5f",
+    "url": "/MovieSearchv2/static/js/runtime~main.5708e6b8.js"
+  },
+  {
+    "revision": "2d1ef347e8b0290a8a860e368f797205",
+    "url": "/MovieSearchv2/static/media/pumpkin.2d1ef347.png"
+  }
+]);
